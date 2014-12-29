90093052964338991566beb936a7e52b82201fe5
Brian Friesen <brianfriesen@quickenloans.com>
2014-12-29 02:20:29 -0500

    Change visibility of members of ExportInfo; expose DefaultPriority.

--- src/StaticDependencyInjection/ExportInfo.Generated.cs
+++ src/StaticDependencyInjection/ExportInfo.Generated.cs
@@ -7,18 +7,21 @@ namespace Rock.StaticDependencyInjection
     /// </summary>
     internal class ExportInfo
     {
-        private const int _defaultPriority = -1;
+        /// <summary>
+        /// The default priority for an instance of <see cref="ExportInfo"/> if not specified.
+        /// </summary>
+        public const int DefaultPriority = -1;
 
         private readonly Type _targetClass;
         private readonly int _priority;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ExportInfo"/> class with
-        /// a priority of negative one.
+        /// a priority with the value of <see cref="DefaultPriority"/>.
         /// </summary>
         /// <param name="targetClass">The class to be exported.</param>
-        internal ExportInfo(Type targetClass)
-            : this(targetClass, _defaultPriority)
+        public ExportInfo(Type targetClass)
+            : this(targetClass, DefaultPriority)
         {
         }
 
@@ -29,7 +32,7 @@ namespace Rock.StaticDependencyInjection
         /// <param name="priority">
         /// The priority of the export, relative to the priority of other exports.
         /// </param>
-        internal ExportInfo(Type targetClass, int priority)
+        public ExportInfo(Type targetClass, int priority)
         {
             if (targetClass == null)
             {
@@ -48,26 +51,26 @@ namespace Rock.StaticDependencyInjection
         /// <summary>
         /// Gets the class to be exported.
         /// </summary>
-        internal Type TargetClass { get { return _targetClass; } }
+        public Type TargetClass { get { return _targetClass; } }
 
         /// <summary>
         /// Gets the priority of the export, relative to the priority of other exports.
         /// The default value, if not specified in the constructor is negative one.
         /// This value is used during import operations to sort the discovered classes.
         /// </summary>
-        internal int Priority { get { return _priority; } }
+        public int Priority { get { return _priority; } }
 
         /// <summary>
         /// Gets or sets the name of the export. This value is compared against the
         /// name parameter in various import operations.
         /// </summary>
-        internal string Name { get; set; }
+        public string Name { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether the type indicated by
         /// <see cref="TargetClass"/> should be excluded from consideration during an
         /// import operation.
         /// </summary>
-        internal bool Disabled { get; set; }
+        public bool Disabled { get; set; }
     }
 }
